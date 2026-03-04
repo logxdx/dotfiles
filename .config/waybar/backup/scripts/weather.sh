775a68c9a43e243e03ca21ae0718b93b79a038fb
logx <iamvarun158@gmail.com>
2026-03-05 02:59:54 +0530

    Backup 2026-03-05 02:59:54

--- .config/waybar/backup/scripts/weather.sh
+++ .config/waybar/backup/scripts/weather.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
-BSSIDS="$(nmcli device wifi list |
-    awk 'NR>1 {if ($1 != "*") {print $1}}' |
-    tr -d ":" |
-    tr "\n" ",")"
 
 LOC=""
 REQUEST_GEO="$(wget -qO - http://openwifi.su/api/v1/bssids/"$BSSIDS")"
@@ -16,7 +12,7 @@ text="$(curl -s "https://wttr.in/$LOC?format=1" | sed 's/ //g')"
 tooltip="$(curl -s "https://wttr.in/$LOC?0QT" |
     sed 's/\\/\\\\/g' |
     sed ':a;N;$!ba;s/\n/\\n/g' |
-    sed 's/"/\\"/g')"
+    sed 's/"/\\"g')"
 
 if ! grep -q "Unknown location" <<< "$text"; then
     echo "{\"text\": \"$text\", \"tooltip\": \"<tt>$tooltip</tt>\", \"class\": \"weather\"}"
